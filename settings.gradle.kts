c9d041c0fdddcffbde45b37ca1a31b3a48b6c7a5
Brice Dutheil <brice.dutheil@gmail.com>
2025-09-17 13:07:26 +0200

    chore: Don't auto approve terms of use for buildScan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,20 +7,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
-    id("com.gradle.develocity") version "4.1.1"
-}
-
-develocity {
-    buildScan {
-        termsOfUseUrl = "https://gradle.com/terms-of-service"
-        termsOfUseAgree = "yes"
-        // publish on failure only when NOT running on CI
-        publishing {
-            onlyIf {
-                it.buildResult.failures.isNotEmpty() && !providers.environmentVariable("CI").isPresent
-            }
-        }
-    }
+    id("com.gradle.develocity") version "4.2"
 }
 
 rootProject.name = "jufmt"
@@ -30,3 +17,17 @@ include(
 )
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+        // termsOfUseAgree is handled by .gradle/init.d/configure-develocity.init.gradle.kts
+
+        // Only publish on failure when NOT running on CI
+        publishing {
+            onlyIf {
+                it.buildResult.failures.isNotEmpty() && !providers.environmentVariable("CI").isPresent
+            }
+        }
+    }
+}
